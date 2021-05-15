b923cb8db182ea8d4717c7b12775f3a7f417aa11
ChadDiaz <chadcdiaz@gmail.com>
2021-05-14 22:10:34 -0400

    finished

---
+++ calphabeticShift.py
@@ -0,0 +1,40 @@
+'''
+Given a string, your task is to replace each of its characters by the next one in the English alphabet; i.e. replace a with b, replace b with c, etc (z would be replaced by a).
+
+Example
+
+For inputString = "crazy", the output should be alphabeticShift(inputString) = "dsbaz".
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] string inputString
+
+A non-empty string consisting of lowercase English characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 1000.
+
+[output] string
+
+The resulting string after replacing each of its characters.
+
+PLAN:
+Transform each character to uni. Add 1. Unless character is 122(z), return 97(a).
+newList = ""
+for index, letter in enumerate(input_string)
+at each index, change the letter to a uni character, increase each uni by 1, assign it to the newList, when finished, take each new uni and change it to the chr(uni), then return str(newList)
+'''
+
+def alphabeticShift(inputString):
+    newList = ""
+
+    for char in inputString:
+        if char == "z":
+            newList += "a"
+        else:
+            newList += chr(ord(char) +1)
+    return newList
+
+    
---
+++ csWorstCastTimeAppendingDynamicArray.py
@@ -0,0 +1,8 @@
+"""
+In your own words, explain why the worst-case time cost of appending to a dynamic array is O(0)
+
+A -  
+when you have to append the array, you first have to double it, then take the time to copy the data from the old array to the new array before you can add your new data to the new array. 
+
+
+""" 
